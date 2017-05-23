e2ab1ab09e185a4392b61db2e96a64e0c6cc4d0d
modernosvb <moderno@gruposvb.com.br>
2017-05-22 23:44:46 -0300

    Versão inicial

---
+++ Prototype/Web/src/app/scripts/app.js
@@ -0,0 +1,50 @@
+﻿'use strict';
+
+/**
+ * @ngdoc overview
+ * @name prisappApp
+ * @description
+ * # prisappApp
+ *
+ * Main module of the application.
+ */
+angular
+    .module('prisappApp', [
+        'ngAnimate',
+        'ngAria',
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngMaterial'
+    ])
+    .config(function ($routeProvider, $locationProvider) {
+
+        $locationProvider.hashPrefix('');
+
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/about', {
+                templateUrl: 'views/about.html',
+                controller: 'AboutCtrl',
+                controllerAs: 'about'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    })
+    .controller('AppCtrl', function ($scope, $timeout, $mdSidenav) {
+        $scope.toggleLeft = buildToggler('left');
+        $scope.toggleRight = buildToggler('right');
+
+        function buildToggler(componentId) {
+            return function () {
+                $mdSidenav(componentId).toggle();
+            };
+        }
+    });
---
+++ Prototype/Web/src/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name prisappApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the prisappApp
+ */
+angular.module('prisappApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
