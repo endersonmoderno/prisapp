1e60b7cd7411ccfe37af5c6f8c1636c8718a4539
modernosvb <moderno@gruposvb.com.br>
2017-05-28 20:08:00 -0300

    tela de conquistas

--- Prototype/Web/src/app/scripts/controllers/conquistas.js
+++ Prototype/Web/src/app/scripts/controllers/conquistas.js
@@ -1,4 +1,4 @@
-'use strict';
+﻿'use strict';
 
 /**
  * @ngdoc function
@@ -8,10 +8,45 @@
  * Controller of the prisappApp
  */
 angular.module('prisappApp')
-  .controller('ConquistasCtrl', function () {
+    .controller('ConquistasCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.items = [
+        {
+            login: '@maria.alice',
+            valor: '1380',
+            foto: 'images/mariaalice.png'
+        },
+        {
+            login: '@enderson.moderno',
+            valor: '1300',
+            foto: 'images/enderson.png',
+            ativo: 'sim'
+        },
+        {
+            login: '@alexandre.paiva',
+            valor: '1110',
+            foto: 'images/alexandre.png'
+        },
+        {
+            login: '@renata.bischoff',
+            valor: '860',
+            foto: 'images/renata.png'
+        },
+        {
+            login: '@pedro.geraldine',
+            valor: '200',
+            foto: 'images/pedro.png'
+        },
+        {
+            login: '@marcello.piola',
+            valor: '0',
+            foto: 'images/piola.png'
+        }
+    ];
+
   });
